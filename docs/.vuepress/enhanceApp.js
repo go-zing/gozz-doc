067c913d24bff09391098396b8f51af95ca521bc
Maple.Wu <lingfeng.wu@mihoyo.com>
2023-10-20 14:34:42 +0800

    feat(docs): update examples

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,14 +1,16 @@
 export default ({router}) => {
     router.onReady(() => {
-        const {hash} = document.location;
-        const h = decodeURIComponent(hash.substring(1))
-        if (h) {
-            setTimeout(() => {
-                const el = document.getElementById(h);
-                if (el) {
-                    el.scrollIntoView()
-                }
-            }, 200)
+        if (typeof window !== 'undefined') {
+            const {hash} = document.location;
+            const h = decodeURIComponent(hash.substring(1))
+            if (h) {
+                setTimeout(() => {
+                    const el = document.getElementById(h);
+                    if (el) {
+                        el.scrollIntoView()
+                    }
+                }, 200)
+            }
         }
     });
 };
