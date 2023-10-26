f41352425e730f00acb0cbdbc42811ed8e247e33
Maple Wu <31134320+Just-maple@users.noreply.github.com>
2023-10-26 19:24:46 +0800

    feat(updaet): swagger

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -48,7 +48,11 @@ function getNavSidebar(lang, home, guide, story, plugin) {
 
 module.exports = {
     head: [
-        ['meta', {name: 'viewport', content: 'width=device-width, initial-scale=1'}],
+        ['meta', {
+            name: 'keywords',
+            content: 'Golang,Golang Framework,Golang Tools,Golang Annotation,Golang DI,Dependency Inject,'
+        }],
+        ['meta', {name: 'viewport', content: 'width=device-width,initial-scale=1'}],
         ['link', {rel: 'icon', href: '/favicon.ico'}],
     ],
     title: 'GOZZ',
