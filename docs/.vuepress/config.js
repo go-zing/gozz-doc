73d50b5c62121c018549ba7d11d4f7f2ae5c489e
Maple Wu <31134320+Just-maple@users.noreply.github.com>
2023-10-26 19:24:56 +0800

    feat(updaet): swagger

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -52,7 +52,7 @@ module.exports = {
             name: 'keywords',
             content: 'Golang,Golang Framework,Golang Tools,Golang Annotation,Golang DI,Dependency Inject,'
         }],
-        ['meta', {name: 'viewport', content: 'width=device-width,initial-scale=1'}],
+        ['meta', {name: 'viewport', content: 'width=device-width, initial-scale=1'}],
         ['link', {rel: 'icon', href: '/favicon.ico'}],
     ],
     title: 'GOZZ',
